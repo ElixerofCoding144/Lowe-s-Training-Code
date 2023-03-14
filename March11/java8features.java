8b86a25ce73bb5c8bd5d8bac9154ee4bf766c62c
Antara Chakrabarti <56504745+ElixerofCoding144@users.noreply.github.com>
2023-03-14 11:29:03 +0530

    Add files via upload

---
+++ March11/java8features.java
@@ -0,0 +1,11 @@
+import java.util.*;
+/*
+ Functional Interface -- having only 1 abstract method
+ @functional interface
+ Syntax:
+ (argument list) -> {body} ; here -> links argument list and body; body is a set of codes
+ 
+ */
+public class java8features {
+
+}
