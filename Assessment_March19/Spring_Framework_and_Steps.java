957287e44f226645042ad3679b446786e41deee9
Antara Chakrabarti <56504745+ElixerofCoding144@users.noreply.github.com>
2023-03-24 16:54:36 +0530

    Add files via upload

---
+++ Assessment_March19/Spring_Framework_and_Steps.java
@@ -0,0 +1,34 @@
+//-----------------------------------------------------SPRING--------------------------------------------------------------------
+//      Father of Spring framework - Rod Johnson
+//		Light weight Open source Framework
+//      Released in October 2002
+//		Easily develop web applications
+//		Spring Framework is based on the principles of -
+//			1. dependency injection (DI) 2. inversion of control (IoC)
+//      These principles reduce coupling between components and increase modularity and maintainability of applications.
+//      We have two types of dependency injections - 
+//			1. Setter Based 
+//			2. Constructor Based
+//		Spring Framework is a modular framework and consists of several modules.
+//		Spring JDBC: This module provides support for JDBC (Java Database Connectivity) and simplifies database access using JDBC.
+//		Spring ORM: This module provides integration with Object-Relational Mapping (ORM) frameworks such as Hibernate, JPA, and iBatis.
+//		Spring Web: This module provides support for building web applications using Spring MVC (Model-View-Controller) and other web-related features such as RESTful services, WebSocket, and Servlets.
+//		Spring Security: This module provides security features for Spring-based applications, including authentication, authorization, and access control.
+//		Spring Integration: This module provides support for building message-driven architectures and integrating with other systems using messaging channels, endpoints, and adapters.
+//		Spring Batch: This module provides support for batch processing and allows developers to build batch jobs that can handle large volumes of data.
+//		Spring Data: This module provides integration with different data stores such as MongoDB, Cassandra, and Redis. It also provides a unified programming model for data access and simplifies working with data in Spring-based applications.
+//		Spring MVC Spring JDBC Spring JPA Spring Security
+		
+//		------------------------------------STEPS TO DEVELOP SPRING PROJECT--------------------------------------------------------------
+//		1. Create Java Project 
+//		2. Load the drivers 
+//		3. Create bean class 
+//		4. Create xml file to get the properties 
+//		5. Create test class
+		
+//		Resource is AncestorEvent interfacEvent from org.springframework.com.io.Resource package
+//		Resource represents the information of xml file
+//		ClassPathResource is an implementation class of Resource Interface from org.springframework.core.io package
+//		To return the bean, we have BeanFactory. BeanFactory is an interface which is from org.springframework.beans.factory
+//		XmlBeanFactory is an implementation class of BeanFactory from org.springframework.beans.factory package
+
