a3d26c1337828deebc368331ee4b9e47c67a1705
Antara Chakrabarti <56504745+ElixerofCoding144@users.noreply.github.com>
2023-03-24 19:48:36 +0530

    Create Product.java

---
+++ March18/SpringApplication/src/com.antara/Product.java
@@ -0,0 +1,22 @@
+package com.antara;
+
+public class Product 
+{
+	String pName;
+	//setter method
+//	public void setpName(String pName) {
+//	this.pName = pName;
+//}
+	
+	public void show()
+	{
+		System.out.println("Thanks for purchasing "+pName);
+	}
+	//using constructor
+	public Product(String pName) {
+		this.pName = pName;
+	}
+
+
+	
+}
